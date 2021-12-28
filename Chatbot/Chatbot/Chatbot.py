891c7718a2aea9b9a56f5a8246cc1bcd7d7b7414
NOEL PAUL GEORGE <75027970+Noel6161131110@users.noreply.github.com>
2021-12-28 11:22:27 +0530

    Update Chatbot.py
    edited by noel at 11:22 AM in 28/12/2021

--- Chatbot/Chatbot/Chatbot.py
+++ Chatbot/Chatbot/Chatbot.py
@@ -1,18 +1,22 @@
+#import Chatbot module from chatterbot library 
+#import ChatterBotCorpusTrainer from chatterbot.trainers library
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
-from chatterbot.trainers import ListTrainer
 
-
-
+#created ChatBot names Joel
 chatbot = ChatBot('Joel')
 
 trainer = ChatterBotCorpusTrainer(chatbot)
 
 trainer.train("chatterbot.corpus.english")
 
+print("Hi I am Joel , the Bot ")
+print("Your companion in the entire journey")
+
 while True:
      request = input('You: ')
      response = chatbot.get_response(request)
-     print('Bot: ', response)
+     print('Joel: ', response)
+
 
 
