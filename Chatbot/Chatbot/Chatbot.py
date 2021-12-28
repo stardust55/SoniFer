3a0e00c2d086cf1dfa80d2c2a6f7b3db852d4d3a
NOEL PAUL GEORGE <75027970+Noel6161131110@users.noreply.github.com>
2021-12-28 12:49:11 +0530

    Update Chatbot.py
    edited by Noel 12:50 pm in 28/12/2021

--- Chatbot/Chatbot/Chatbot.py
+++ Chatbot/Chatbot/Chatbot.py
@@ -2,13 +2,30 @@
 #import ChatterBotCorpusTrainer from chatterbot.trainers library
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
+import json
+
+data = json.loads(open('nfL6.json', 'r').read())
+
+train = []
+
+for k, row in enumerate(data):
+    train.append(row['question'])
+    train.append(row['answer'])
 
 #created ChatBot names Joel
 chatbot = ChatBot('Joel')
 
-trainer = ChatterBotCorpusTrainer(chatbot)
+trainer1 = ChatterBotCorpusTrainer(chatbot)
+trainer1.train(
+    "chatterbot.corpus.english.greetings",
+    "chatterbot.corpus.english.conversations"
+)
 
-trainer.train("chatterbot.corpus.english")
+
+trainer = ListTrainer(chatbot)
+
+trainer.train(train[:1024])
 
 print("Hi I am Joel , the Bot ")
 print("Your companion in the entire journey")
@@ -17,6 +34,5 @@ while True:
      request = input('You: ')
      response = chatbot.get_response(request)
      print('Joel: ', response)
-
 
 
